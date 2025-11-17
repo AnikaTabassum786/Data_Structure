fad48394805842ea751f76798a854eea0be75d64
AnikaTabassum217 <tabassum.anika579@gmail.com>
2025-11-17 09:55:07 +0600

    Union and Intersection

--- src/unionAndIntersection.ts
+++ src/unionAndIntersection.ts
@@ -1,3 +1,5 @@
+//union
+
 type UserRole = 'admin'|'user';
 
 const getDashboard = (role:UserRole)=>{
@@ -16,3 +18,36 @@ const getDashboard = (role:UserRole)=>{
 }
 
 getDashboard('admin')
+
+
+//intersection
+
+type Employee={
+  id:string;
+  name:string;
+  phoneNo:string
+}
+
+type Manager={
+  designation:string;
+  teamSize:number;
+}
+
+type EmployeeManager = Employee & Manager
+
+const ChowdhuryShaheb:EmployeeManager={
+   id:'101',
+   name:'Mr Chowdhury',
+   phoneNo:'01312....',
+   designation:'Manager',
+   teamSize:20
+}
+
+type EmployeeOrManager = Employee | Manager
+
+const KhanShaheb:EmployeeOrManager={
+   id:'103',
+   name:'Mr Khan',
+   phoneNo:'01318....',
+   teamSize:10
+}
