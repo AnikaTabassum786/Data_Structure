5ff591a15824d708e161e22e416335777c1803ec
AnikaTabassum217 <tabassum.anika579@gmail.com>
2025-11-18 11:13:48 +0600

    Never type also done

--- src/nullableUnknownNever.ts
+++ src/nullableUnknownNever.ts
@@ -46,3 +46,11 @@ const discountCalculator = (price:unknown) =>{
 discountCalculator(100)
 discountCalculator("100 tk")
 discountCalculator(null)
+
+// void function and never type
+
+const throwError = (msg: string): never =>{
+    throw new Error(msg)
+}
+
+throwError("Error Happened")
