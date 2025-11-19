0df813cc0d153276208751d20f3356019eb79544
AnikaTabassum217 <tabassum.anika579@gmail.com>
2025-11-19 14:28:16 +0600

    Type Generic

---
+++ src/generics.ts
@@ -0,0 +1,46 @@
+//dynamically generalize: Generic
+
+//Example-1
+
+// type GenericStringArray = Array<string>
+// type GenericNumberArray = Array<number>
+// type GenericBooleanArray = Array<boolean>
+
+type GenericArray<T> = Array<T>
+
+
+// const friends: string[] = [ "Mila","Kaniz","Pushpo"]
+// const friends: Array<string> = [ "Mila","Kaniz","Pushpo"]
+const friends: GenericArray<string> = [ "Mila","Kaniz","Pushpo"]
+
+
+// const rollNumber: number[] = [2,3,5]
+// const rollNumber:Array<number> = [2,3,5]
+const rollNumber:GenericArray<Number> = [2,3,5]
+
+// const isEligibleList:boolean[] = [true,false,true]
+// const isEligibleList:Array<boolean> = [true,false,true]
+const isEligibleList:GenericArray<boolean> = [true,false,true]
+
+//Example-2
+
+type Coordinates<T,M> = [T,M]
+
+const coordinates1:Coordinates<number,number> = [20,30]
+const coordinates2:Coordinates<string,string> =['20','30']
+// console.log(coordinates1,coordinates2)
+
+//Example-3- Array of object
+
+const userList: GenericArray<{name:string,age:number}>=[
+    {
+        name:'X',
+        age:10
+    },
+    {
+        name:'Y',
+        age:20
+    }
+]
+console.log(userList)
+
