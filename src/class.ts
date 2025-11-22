67bda78480b29c33ab21fb33946e6dcf25b5146c
AnikaTabassum217 <tabassum.anika579@gmail.com>
2025-11-22 20:45:07 +0600

    Class done

--- src/class.ts
+++ src/class.ts
@@ -1,16 +1,49 @@
-class Animal {
-    name:string;
-    species:string;
-    sound:string;
+// class Animal {
+//     name:string;
+//     species:string;
+//     sound:string;
 
-    constructor( name:string, species:string,sound:string){
-        this.name=name;
-        this.species=species;
-        this.sound=sound
+//     constructor( name:string, species:string,sound:string){
+//         this.name=name;
+//         this.species=species;
+//         this.sound=sound
+//     }
+
+//     makeSound(){
+//         console.log(`This ${this.name} make sound ${this.sound}`)
+//     }
+// }
+
+
+// Parameter Properties
+
+
+
+class Animal {
+
+    //Public key word define property and also initialize property
+
+    constructor( 
+        public name:string,
+        public species:string, 
+        public sound:string){
+    }
+
+    makeSound(){
+        console.log(`This ${this.name} make sound ${this.sound}`)
     }
 }
 
 const dog = new Animal("Sinba","dog","Gau Gau")
+const cat = new Animal("Miko","Cat","Mew.. Mew..")
+
 console.log(dog.name)
 console.log(dog)
+
+console.log('-------------')
+
+cat.makeSound()
+dog.makeSound()
+
+
 
